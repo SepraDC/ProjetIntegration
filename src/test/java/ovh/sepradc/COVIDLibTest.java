75684cb1088a68200536cf964153c9a1c7335ba4
SepraDC <lucas.birac@epsi.fr>
2021-01-29 10:43:37 +0100

    ajout test

--- src/test/java/ovh/sepradc/COVIDLibTest.java
+++ src/test/java/ovh/sepradc/COVIDLibTest.java
@@ -1,6 +1,15 @@
 package ovh.sepradc;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class COVIDLibTest extends TestCase {
+
+    @Test
+    public void testApp() {
+        int expected = 1;
+        int actual = 1;
+
+        assertEquals(expected, actual);
+    }
 }
